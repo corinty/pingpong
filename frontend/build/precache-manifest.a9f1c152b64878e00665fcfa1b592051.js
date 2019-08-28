3f030c211c4b7b82fe440172d731320f22ea1eea
Corin McHargue <corin.mchargue@gmail.com>
2019-08-28 15:40:28 -0500

    yeet

---
+++ frontend/build/precache-manifest.a9f1c152b64878e00665fcfa1b592051.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a4c52306981b3b18dd6afa1bf978911",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8559c28081f7efbdd52",
+    "url": "/static/css/2.d32869f0.chunk.css"
+  },
+  {
+    "revision": "fc201a2151095b4c4e3d",
+    "url": "/static/css/main.7a52e019.chunk.css"
+  },
+  {
+    "revision": "c8559c28081f7efbdd52",
+    "url": "/static/js/2.3565a1f7.chunk.js"
+  },
+  {
+    "revision": "fc201a2151095b4c4e3d",
+    "url": "/static/js/main.77c9aafb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
