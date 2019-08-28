1b87eb8c9cfc432a6a09203adec26db2ae0ea150
Corin McHargue <corin.mchargue@gmail.com>
2019-08-28 14:33:06 -0500

    messed up the homepage

---
+++ frontend/build/precache-manifest.8948f8bf35247d9d79371cbd11b1c628.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efdba31f809ca5b10f53ef28a4800373",
+    "url": "/pingpong/index.html"
+  },
+  {
+    "revision": "8c308e2cf690cc99148d",
+    "url": "/pingpong/static/css/2.d32869f0.chunk.css"
+  },
+  {
+    "revision": "012ff032532cb889d714",
+    "url": "/pingpong/static/css/main.7a52e019.chunk.css"
+  },
+  {
+    "revision": "8c308e2cf690cc99148d",
+    "url": "/pingpong/static/js/2.94a4b0a2.chunk.js"
+  },
+  {
+    "revision": "012ff032532cb889d714",
+    "url": "/pingpong/static/js/main.0573168c.chunk.js"
+  },
+  {
+    "revision": "f2c1b2130f99257e6e46",
+    "url": "/pingpong/static/js/runtime~main.cd9e352b.js"
+  }
+]);
