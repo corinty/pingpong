ca718b1f74cd4a3614786cdbc9388e0b14013513
Corin McHargue <corin.mchargue@gmail.com>
2019-08-28 13:59:46 -0500

    production build

---
+++ frontend/build/precache-manifest.16a6088486b90379113bb13943332182.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07be5fd91b908d5fe45a312d079029cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8559c28081f7efbdd52",
+    "url": "/static/css/2.d32869f0.chunk.css"
+  },
+  {
+    "revision": "e6c6e616d87e195a39a0",
+    "url": "/static/css/main.7a52e019.chunk.css"
+  },
+  {
+    "revision": "c8559c28081f7efbdd52",
+    "url": "/static/js/2.3565a1f7.chunk.js"
+  },
+  {
+    "revision": "e6c6e616d87e195a39a0",
+    "url": "/static/js/main.07f94b21.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
